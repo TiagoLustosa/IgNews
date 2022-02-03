52e576f87530045a12a964d0ad2ec8800664e3c9
Tiago Lustosa Santos <tiagolustosasantos@hotmail.com>
2022-02-03 07:41:25 -0300

    Erro com fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,27 @@
-import NextAuth from "next-auth"
+import { query as q } from 'faunadb'
+import NextAuth, { Account, CallbacksOptions, Profile, User } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+import { FaunaAdapter } from '@next-auth/fauna-adapter'
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      authorization: 'https://github.com/login/oauth/authorize?scope=read:user'
-    }),
-  ],
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+            authorization: 'https://github.com/login/oauth/authorize?scope=read:user+user:email'
+        }),
+    ],
+    adapter: FaunaAdapter(fauna),
+    callbacks: {
+        async signIn(user) {
+            const { email } = user
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { email } }
+                ))
+            return true
+        }
+    }
 })
